a4962b0ab390a2e1c70d8b013377a1e67bf51034
Dejan Pangercic <pangercic@lapradig41.informatik.tu-muenchen.de>
2009-07-01 12:01:47 +0200

    full print of plane_cluster_service return

--- plane_clusters/src/query_service.cpp
+++ plane_clusters/src/query_service.cpp
@@ -18,6 +18,20 @@ int main(int argc, char **argv)
       {
         //add response variables of choice
         ROS_INFO("Coeff a: %f", srv.response.a);
+        ROS_INFO("Coeff b: %f", srv.response.b);
+        ROS_INFO("Coeff c: %f", srv.response.c);
+        ROS_INFO("Coeff d: %f", srv.response.d);
+        ROS_INFO("pcenter X: %f", srv.response.pcenter.x);
+        ROS_INFO("pcenter Y: %f", srv.response.pcenter.y);
+        ROS_INFO("pcenter Z: %f", srv.response.pcenter.z);
+        for (int i = 0; i < srv.response.oclusters.size(); i++)
+          {
+            ROS_INFO("OBJECT: %d", i);
+            ROS_INFO("ocluster X: %f", srv.response.oclusters[1].center.x);
+            ROS_INFO("ocluster Y: %f", srv.response.oclusters[2].center.y);
+            ROS_INFO("ocluster Z: %f", srv.response.oclusters[3].center.z);
+
+          }
       }
     else
     {
