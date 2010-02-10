a9a4d3f1c483e06e4f25d7fe7c8aa32357dc2751
Nico Blodow <blodow@cs.tum.edu>
2010-02-10 15:09:30 +0100

    ias_sample_consensus and region growing

---
+++ cloud_algos/include/cloud_algos/region_growing.h
@@ -0,0 +1,90 @@
+#ifndef CLOUD_ALGOS_REGION_GROWING_H
+#define CLOUD_ALGOS_REGION_GROWING_H
+#include <point_cloud_mapping/kdtree/kdtree_ann.h>
+#include <cloud_algos/cloud_algos.h>
+
+/** This class implements Region Growing. It segments a point cloud */
+class RegionGrowing : public CloudAlgo
+{
+ public:
+  /** this struct allows for different stopping criteria (where region growing 
+      should stop) */
+  struct StopAt {
+    /** stop method to decide over a given point. This method should be 
+        overriden to implement new stopping criteria */
+    bool stop (sensor_msgs::PointCloudConstPtr &cloud, int i, int seed)
+    {
+      return false;
+    }
+  };
+  /** region growing should stop at points with dimension / channel values 
+      higher than a user defined threshold */
+  struct StopAtHighDimension : StopAt {
+    /** set threshold value (maximum accepted value) */
+    void setThreshold (double thresh) {max_val_ = thresh;}
+    /** set threshold dimension (maximum accepted value) */
+    void setChannelDimension (int d) {chIdx_ = d;}
+    /** overriden stop method to decide over a given point */
+    bool stop (sensor_msgs::PointCloudConstPtr &cloud, int i, int seed)
+    {
+      return cloud->channels.at (chIdx_).values.at (i) > max_val_;
+    }
+    double max_val_;
+    int chIdx_;
+  };
+  /** region growing should stop at points which differ from a given seed point in a 
+      given dimension / channel value more than a user defined threshold */
+  struct StopAtDimensionDifference : StopAt {
+    /** set threshold value (maximum accepted value) */
+    void setThreshold (double thresh) {max_val_ = thresh;}
+    /** set threshold dimension (maximum accepted value) */
+    void setChannelDimension (int d) {chIdx_ = d;}
+    /** overriden stop method to decide over a given point */
+    bool stop (sensor_msgs::PointCloudConstPtr &cloud, int i, int seed)
+    {
+      return fabs (cloud->channels.at (chIdx_).values.at (i) - cloud->channels.at (chIdx_).values.at (i)) > max_val_;
+    }
+    double max_val_;
+    int chIdx_;
+  };
+
+  typedef sensor_msgs::PointCloud OutputType;
+
+  std::string default_topic_name () 
+    {return std::string ("cloud_pcd");};
+
+  std::string default_node_name () 
+    {return std::string ("region_growing_node");};
+
+  void init (ros::NodeHandle&);
+  std::vector<std::string> pre  ();
+  std::vector<std::string> post ();
+  std::string process (sensor_msgs::PointCloudConstPtr&);
+  OutputType output ();
+  
+  void GrowFromPoint (sensor_msgs::PointCloudConstPtr&, int);
+  void OptimizedRegionGrowing (sensor_msgs::PointCloudConstPtr&);
+  void OptimizedRegionGrowing (sensor_msgs::PointCloudConstPtr&, std::vector<int>);
+
+  void setStopAtCriterion (StopAt);
+  void setRadius (double radius);
+  void setExtraDimensionIgnore (int dimIdx, double dimVal);
+  void setMaxNrNN (int max_nr_nn);
+  
+  double radius_;
+  int dimIdx_;
+  double dimVal_;
+  int max_nr_nn_;
+
+  cloud_kdtree::KdTree* kdtree_;
+  std::vector<sensor_msgs::PointCloud> clusters_;
+  std::vector<int> regions_; // ?? 
+  bool create_pcds_;
+  int regInitial_;
+  int regId_;
+  ros::NodeHandle nh_;
+  StopAt stop_at_;
+};
+
+#endif
+
---
+++ ias_sample_consensus/include/ias_sample_consensus/sac_model_rotational.h
@@ -0,0 +1,66 @@
+#ifndef IAS_SAMPLE_CONSENSUS_SACMODELROTATIONAL_H_
+#define IAS_SAMPLE_CONSENSUS_SACMODELROTATIONAL_H_
+
+#include <point_cloud_mapping/sample_consensus/sac_model.h>
+#include <mapping_msgs/PolygonalMap.h>
+
+namespace ias_sample_consensus
+{
+  /** \brief A Sample Consensus Model class for 3D plane segmentation.
+    */
+  class SACModelRotational : public sample_consensus::SACModel
+  {
+    public:
+      //////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+      /** \brief Constructor for SACModelRotational. */
+      SACModelRotational (mapping_msgs::PolygonalMap& pmap) : pmap_(pmap) 
+      { 
+        nx_idx_ = ny_idx_ = nz_idx_ = -1;
+      }
+
+      //////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+      /** \brief Destructor for SACModelRotational. */
+      virtual ~SACModelRotational () { }
+
+      virtual void getSamples (int &iterations, std::vector<int> &samples);
+
+      //////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+      /** \brief Test whether the given model coefficients are valid given the input point cloud data.
+        * \param model_coefficients the model coefficients that need to be tested
+        * \todo implement this
+        */
+      bool testModelCoefficients (const std::vector<double> &model_coefficients) { return true; }
+
+      virtual bool computeModelCoefficients (const std::vector<int> &samples);
+
+      virtual void refitModel (const std::vector<int> &inliers, std::vector<double> &refit_coefficients);
+      virtual void getDistancesToModel (const std::vector<double> &model_coefficients, std::vector<double> &distances);
+      virtual void selectWithinDistance (const std::vector<double> &model_coefficients, double threshold, std::vector<int> &inliers);
+
+      virtual void projectPoints (const std::vector<int> &inliers, const std::vector<double> &model_coefficients, sensor_msgs::PointCloud &projected_points);
+
+      virtual void projectPointsInPlace (const std::vector<int> &inliers, const std::vector<double> &model_coefficients);
+      virtual bool doSamplesVerifyModel (const std::set<int> &indices, double threshold);
+
+      static int functionToOptimize (void *p, int m, int n, const double *x, double *fvec, int iflag);
+
+      //////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+      /** \brief Return an unique id for this model (666 for now). */
+      virtual int getModelType () { return (666); }
+      
+      static int functionToOptimizeAxis (void *p, int m, int n, const double *x, double *fvec, int iflag);
+      bool MinimizeAxisDistancesToSamples (const std::vector<int> samples, std::vector<double> &model_coefficients, double &err);
+      double PointToRotationalDistance (const std::vector<double> &model_coefficients, const geometry_msgs::Point32 &p);
+      bool EstimateAxisFromSamples (const std::vector<int> samples, std::vector<double> &model_coefficients);
+      void EstimateContourFromSamples (const std::vector<int> samples, std::vector<double> &model_coefficients);
+      bool RefitAxis (const std::vector<int> &inliers, std::vector<double> &refit_coefficients);
+      void RefitContour (const std::vector<int> &inliers, std::vector<double> &refit_coefficients);
+      mapping_msgs::PolygonalMap& pmap_;
+      int nx_idx_;
+      int ny_idx_;
+      int nz_idx_;
+      const std::vector<int> *tmp_inliers_;
+  };
+}
+
+#endif
