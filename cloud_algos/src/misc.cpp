bb429791e66f66d4a19a17d9fbb5b39a87074c12
Nico Blodow <blodow@lapradig68.(none)>
2010-03-18 18:16:16 +0100

    forgot to add file

---
+++ cloud_algos/src/misc.cpp
@@ -0,0 +1,25 @@
+#include <cloud_algos/cloud_algos.h>
+
+//instead of maintaining a patch to ROS, we provide a copy of this function
+int
+  cloud_algos::getChannelIndex (const sensor_msgs::PointCloud &points, std::string value)
+{
+  // Get the index we need
+  for (unsigned int d = 0; d < points.get_channels_size (); d++)
+    if (points.channels[d].name == value)
+      return (d);
+
+  return (-1);
+}
+//instead of maintaining a patch to ROS, we provide a copy of this function
+int
+  cloud_algos::getChannelIndex (const boost::shared_ptr<const sensor_msgs::PointCloud> points, std::string value)
+{
+  // Get the index we need
+  for (unsigned int d = 0; d < points->get_channels_size (); d++)
+    if (points->channels[d].name == value)
+      return (d);
+
+  return (-1);
+}
+
