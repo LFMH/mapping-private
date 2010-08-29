69f500bac6ae08cc3770e613c64e6f80c1a1cf12
Hozefa Indorewala <indorewala@ias.in.tum.de>
2010-08-29 20:57:02 +0200

    Created new point type for segmentation with labeling feature. Implemented basic segmentation functionality

--- pointcloud_segmentation/include/pointcloud_segmentation/pointcloud_segmentation.h
+++ pointcloud_segmentation/include/pointcloud_segmentation/pointcloud_segmentation.h
@@ -2,6 +2,15 @@
 #include <sensor_msgs/PointCloud2.h>
 #include <pcl/io/pcd_io.h>
 
+//Segmentation includes
+#include "pcl/sample_consensus/method_types.h"
+#include "pcl/sample_consensus/model_types.h"
+#include "pcl/segmentation/sac_segmentation.h"
+#include "pcl/ModelCoefficients.h"
+
+//Point type
+#include "pointcloud_segmentation/pointcloud_segmentation_point_types.h"
+
 class PointCloudSegmentation
 {
   public:
@@ -10,10 +19,20 @@ class PointCloudSegmentation
     void pointcloudSegmentationCallBack(const sensor_msgs::PointCloud2& msg);
     void run();
 
+    void publishPointCloud(pcl::PointCloud<pcl::PointSegmentation> pointcloud);
+    void publishPointCloud(pcl::PointCloud<pcl::PointNormal> pointcloud);
+    void segmentPointCloud(pcl::PointCloud<pcl::PointNormal> &pointcloud);
   private:
     ros::NodeHandle nh_;
     std::string subscribe_pointcloud_topic_, frame_id_;
     ros::Subscriber pointcloud_subscriber_;
+    ros::Publisher pointcloud_publisher_;
+    ros::Publisher segment_publisher_;
+    // Create the segmentation object
+    pcl::SACSegmentation<pcl::PointNormal> seg_;
+    Eigen::Vector3f axis_;
+    int label_;
+
 };
 
 
--- pointcloud_segmentation/src/pointcloud_segmentation.cpp
+++ pointcloud_segmentation/src/pointcloud_segmentation.cpp
@@ -1,11 +1,39 @@
 #include <pointcloud_segmentation/pointcloud_segmentation.h>
 
+pcl::PointCloud<pcl::PointNormal> updatePointCloud(pcl::PointCloud<pcl::PointNormal> cloud, pcl::PointIndices inliers)
+{
+  int check = 0;
+  pcl::PointCloud<pcl::PointNormal> ret_cloud;
+
+  for(u_int i = 0; i < cloud.points.size(); i++)
+  {
+    for(u_int j = 0 ; i < inliers.indices.size(); j++)
+    {
+      if( i == inliers.indices[j])
+      {
+        check = 1;
+        break;
+      }
+    }
+    if(check == 0)
+    {
+      ret_cloud.points.push_back(cloud.points[i]);
+    }
+    else
+      check = 0;
+  }
+
+  return (ret_cloud);
+}
 
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 PointCloudSegmentation::PointCloudSegmentation(): nh_("~")
 {
   nh_.param("subscribe_pointcloud_topic", subscribe_pointcloud_topic_, std::string("/point_cloud"));
+  axis_[0] = 0; axis_[1] = 0.5; axis_[2] = 0;
+  label_ = 0;
+  ROS_INFO("pointcloud_segmentation node is up and running.");
   run();
 }
 
@@ -21,15 +49,103 @@ PointCloudSegmentation::~PointCloudSegmentation()
 void PointCloudSegmentation::run()
 {
   pointcloud_subscriber_ = nh_.subscribe(subscribe_pointcloud_topic_, 100, &PointCloudSegmentation::pointcloudSegmentationCallBack, this);
+  segment_publisher_ = nh_.advertise<sensor_msgs::PointCloud2>("segmented_plane", 100);
+  pointcloud_publisher_ = nh_.advertise<sensor_msgs::PointCloud2>("unsegmented_cloud", 100);
   ros::spin();
 }
 
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
+void PointCloudSegmentation::publishPointCloud(pcl::PointCloud<pcl::PointNormal> pointcloud)
+{
+  sensor_msgs::PointCloud2 pointcloud2_msg;
+  pointcloud.width = 1;
+  pointcloud.height = pointcloud.points.size();
+  pcl::toROSMsg(pointcloud, pointcloud2_msg);
+  pointcloud2_msg.header.frame_id = frame_id_;
+  pointcloud2_msg.header.stamp = ros::Time::now();
+  pointcloud_publisher_.publish(pointcloud2_msg);
+}
+
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+void PointCloudSegmentation::publishPointCloud(pcl::PointCloud<pcl::PointSegmentation> pointcloud)
+{
+  sensor_msgs::PointCloud2 pointcloud2_msg;
+  pcl::toROSMsg(pointcloud, pointcloud2_msg);
+  pointcloud2_msg.header.frame_id = frame_id_;
+  pointcloud2_msg.header.stamp = ros::Time::now();
+  segment_publisher_.publish(pointcloud2_msg);
+}
+
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+void PointCloudSegmentation::segmentPointCloud(pcl::PointCloud<pcl::PointNormal> &input_cloud)
+{
+  ROS_INFO("Segmenting Point Cloud of size %d", (int) input_cloud.points.size());
+  label_ ++;
+  pcl::ModelCoefficients coefficients;
+  pcl::PointIndices inliers;
+  pcl::PointCloud<pcl::PointSegmentation> segmented_plane;
+
+  seg_.setOptimizeCoefficients (true);
+  seg_.setModelType (pcl::SACMODEL_PLANE);
+  seg_.setMethodType (pcl::SAC_RANSAC);
+  seg_.setDistanceThreshold (0.01);
+  seg_.setAxis(axis_);
+
+  seg_.setInputCloud (boost::make_shared<pcl::PointCloud<pcl::PointNormal> > (input_cloud));
+  seg_.segment(inliers, coefficients);
+
+  if (inliers.indices.size () == 0)
+  {
+    ROS_ERROR ("Could not estimate a planar model for the given dataset.");
+    exit(1);
+  }
+
+  segmented_plane.points.resize(inliers.indices.size());
+  for(u_int i = 0 ; i < inliers.indices.size(); i++)
+  {
+    segmented_plane.points[i].x = input_cloud.points[inliers.indices[i]].x;
+    segmented_plane.points[i].y = input_cloud.points[inliers.indices[i]].y;
+    segmented_plane.points[i].z = input_cloud.points[inliers.indices[i]].z;
+    segmented_plane.points[i].normal[0] = input_cloud.points[inliers.indices[i]].normal[0];
+    segmented_plane.points[i].normal[1] = input_cloud.points[inliers.indices[i]].normal[1];
+    segmented_plane.points[i].normal[2] = input_cloud.points[inliers.indices[i]].normal[2];
+    segmented_plane.points[i].curvature = input_cloud.points[inliers.indices[i]].curvature;
+    segmented_plane.points[i].label = label_;
+    //input_cloud.points.erase(input_cloud.points.begin() + inliers.indices[i]);
+  }
+  publishPointCloud(segmented_plane);
+  ROS_INFO("Segmented Plane published");
+  input_cloud = updatePointCloud(input_cloud, inliers);
+  //pcl::PointCloud<pcl::PointNormal> pc;
+  //pcl::copyPointCloud(input_cloud, inliers, pc);
+  //input_cloud = input_cloud - pc;
+  publishPointCloud(input_cloud);
+  ROS_INFO("Current unsegmented point cloud published");
+}
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
 void PointCloudSegmentation::pointcloudSegmentationCallBack(const sensor_msgs::PointCloud2& pointcloud2_msg)
 {
+  //Local variables
+  pcl::PointCloud<pcl::PointNormal> input_cloud;
+  pcl::ModelCoefficients coefficients;
+  pcl::PointIndices inliers;
+
   ROS_INFO("Received a point cloud.");
   frame_id_ = pointcloud2_msg.header.frame_id;
+
+  pcl::fromROSMsg(pointcloud2_msg, input_cloud);
+
+  while( input_cloud.points.size() > 100 )
+  {
+    segmentPointCloud(input_cloud);
+    sleep(2);
+  }
+
+  ROS_INFO("Out of points");
 }
 
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
