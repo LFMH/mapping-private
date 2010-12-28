cb1a178d3efea52aa55a993e4efe42109371f5dd
Dejan Pangercic <dejan.pangercic@cs.tum.edu>
2010-12-28 12:02:32 +0100

    de-registered plugins

--- cloud_algos/src/register_plugins.cpp
+++ cloud_algos/src/register_plugins.cpp
@@ -5,14 +5,9 @@
 #include <cloud_algos/rotational_estimation.h>
 #include <cloud_algos/region_growing.h>
 #include <cloud_algos/cylinder_fit_algo.h>
-#include <cloud_algos/box_fit_algo.h>
-#include <cloud_algos/box_fit2_algo.h>
-#include <cloud_algos/depth_image_triangulation.h>
-#include <cloud_algos/mls_fit.h>
 #include <cloud_algos/radius_estimation.h>
 #include <cloud_algos/noise_removal.h>
 #include <cloud_algos/svm_classification.h>
-#include <cloud_algos/global_rsd.h>
 
 using namespace cloud_algos;
 
@@ -21,15 +16,10 @@ using namespace cloud_algos;
 //PLUGINLIB_REGISTER_CLASS(PlanarEstimation, PlanarEstimation, PCD_Algo)
 
 
-PLUGINLIB_DECLARE_CLASS(cloud_algos, MovingLeastSquares, cloud_algos::MovingLeastSquares, cloud_algos::CloudAlgo);
 PLUGINLIB_DECLARE_CLASS(cloud_algos, RotationalEstimation, cloud_algos::RotationalEstimation, cloud_algos::CloudAlgo);
 PLUGINLIB_DECLARE_CLASS(cloud_algos, RegionGrowing, RegionGrowing, cloud_algos::CloudAlgo);
-PLUGINLIB_DECLARE_CLASS(cloud_algos, DepthImageTriangulation, cloud_algos::DepthImageTriangulation, cloud_algos::CloudAlgo);
-PLUGINLIB_DECLARE_CLASS(cloud_algos, BoxEstimation, cloud_algos::BoxEstimation, cloud_algos::CloudAlgo);
-PLUGINLIB_DECLARE_CLASS(cloud_algos, RobustBoxEstimation, cloud_algos::RobustBoxEstimation, cloud_algos::CloudAlgo);
 PLUGINLIB_DECLARE_CLASS(cloud_algos, CylinderEstimation, cloud_algos::CylinderEstimation, cloud_algos::CloudAlgo);
 PLUGINLIB_DECLARE_CLASS(cloud_algos, SVMClassification, cloud_algos::SVMClassification, cloud_algos::CloudAlgo);
-PLUGINLIB_DECLARE_CLASS(cloud_algos, GlobalRSD, cloud_algos::GlobalRSD, cloud_algos::CloudAlgo);
 PLUGINLIB_DECLARE_CLASS(cloud_algos, StatisticalNoiseRemoval, cloud_algos::StatisticalNoiseRemoval, cloud_algos::CloudAlgo);
 PLUGINLIB_DECLARE_CLASS(cloud_algos, LocalRadiusEstimation, cloud_algos::LocalRadiusEstimation, cloud_algos::CloudAlgo);
 
