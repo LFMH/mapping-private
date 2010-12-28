700e451d189951398ddd8bed452dadb6a006157c
Asako Kanezaki <kanezaki@isi.imi.i.u-tokyo.ac.jp>
2010-12-28 22:05:44 +0100

    Added new shape data and classification demos for them.

---
+++ color_feature_classification/demos_artificial/1.compute_subspace.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+# Example directory containing .pcd files
+DATA=`pwd`/data/
+n=0
+
+# compute a subspace
+for i in `find $DATA -type f \( -iname "noiseless_*.pcd" ! -iname "*GRSD_CCHLAC.pcd" ! -iname "*colorCHLAC.pcd" \) | sort -R`
+do
+    echo $i
+    num=$(printf "%03d" $n)
+    dir_name=$(printf "obj%03d" $n)
+    #echo $dir_name
+    rosrun color_feature_classification computeSubspace_with_rotate c $i -dim 800 -comp pca_result_c/compress_axis -rotate 1 -subdiv 5 pca_result_c/$num
+    rosrun color_feature_classification computeSubspace_with_rotate d $i -dim 800 -comp pca_result_d/compress_axis -rotate 1 -subdiv 5 pca_result_d/$num
+    rosrun color_feature_classification computeSubspace g $i -subdiv 5 pca_result_g/$num
+    rosrun color_feature_classification computeSubspace r $i -subdiv 5 pca_result_r/$num
+    n=`expr $n + 1`
+    #rosrun color_feature_classification computeSubspace r $files -dim 50 -comp pca_result_r/compress_axis pca_result_r/$num
+done
---
+++ color_feature_classification/demos_artificial/2.test_classify.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+# Example directory containing .pcd files
+DATA=`pwd`/data/
+n=0
+
+for i in `find $DATA -type f \( -iname "noisy_*.pcd" ! -iname "*GRSD_CCHLAC.pcd" ! -iname "*colorCHLAC.pcd" \) | sort -R`
+do
+    echo $i
+    num=$(printf "%03d" $n)
+    dir_name=$(printf "obj%03d" $n)
+    #echo $dir_name
+    rosrun color_feature_classification test_classify $i c s -sub 100 -dim 800 -comp pca_result_c/compress_axis
+    #rosrun color_feature_classification test_classify $i d s -sub 100 -dim 800 -comp pca_result_d/compress_axis
+    #rosrun color_feature_classification test_classify $i g s -sub 10 
+    #rosrun color_feature_classification test_classify $i r s -sub 80
+    ### rosrun color_feature_classification test_classify $i r s -sub 10 -dim 50 -comp pca_result_r/compress_axis
+    n=`expr $n + 1`
+done
