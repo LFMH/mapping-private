259c7f262bb88f91abb0ff797f6581b213ecefd9
Dejan Pangercic <dejan.pangercic@gmail.com>
2010-12-30 17:19:49 +0100

    visualize histograms

---
+++ color_feature_classification/demos/3.view_histograms.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# Example directory containing .pcd files
+DATA=`pwd`/hist_data/obj004/1
+
+#for i in `find $DATA -type d -name "*"`
+#do
+#  echo $i
+# for j in `find $DATA -type f \( -iname "*.pcd" \)`
+# do
+#     echo "j: " $j
+#     all_files=`echo $all_files" "$j`
+#     break
+# done
+#done
+
+# echo "all_files: " $all_files
+files=`find $DATA -type f \( -iname "*.pcd" ! -iname "*vfh*" ! -iname "*colorCHLAC*" \)`
+rosrun pcl_visualization pcd_viewer $files
