5e7fb87578e9a6da88a7892f3b346902277dd249
Dejan Pangercic <dejan.pangercic@gmail.com>
2011-01-11 20:53:05 +0100

    moved to scripts directory

---
+++ color_feature_classification/scripts/demos/0.compute_compress_axis.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+# Example directory containing .pcd files
+DATA=`rospack find color_feature_classification`/demos
+
+# compute a subspace
+files=`find $DATA/features_c/* -type f -iname "*.pcd" | sort -d`
+rosrun color_feature_classification computeSubspace_from_file $files pca_result_c/compress_axis
+files=`find $DATA/features_d/* -type f -iname "*.pcd" | sort -d`
+rosrun color_feature_classification computeSubspace_from_file $files pca_result_d/compress_axis
+files=`find $DATA/features_g/* -type f -iname "*.pcd" | sort -d`
+rosrun color_feature_classification computeSubspace_from_file $files pca_result_g/compress_axis
+files=`find $DATA/features_r/* -type f -iname "*.pcd" | sort -d`
+rosrun color_feature_classification computeSubspace_from_file $files pca_result_r/compress_axis
+
+# files=`find $DATA -type f \( -iname "*.pcd" ! -iname "*vfh*" ! -iname "*colorCHLAC*" \)`
+# rosrun color_feature_classification computeSubspace_with_rotate c $files -rotate 1 -subdiv 10 -offset 5 pca_result_c/compress_axis
+# rosrun color_feature_classification computeSubspace_with_rotate d $files -rotate 1 -subdiv 10 -offset 5 pca_result_d/compress_axis
+# rosrun color_feature_classification computeSubspace g $files -subdiv 10 -offset 5 pca_result_g/compress_axis
+# rosrun color_feature_classification computeSubspace r $files -subdiv 10 -offset 5 pca_result_r/compress_axis
