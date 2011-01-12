dcd354462a7a0ec98ad7763ee48ee7b4c0bda9cd
Dejan Pangercic <dejan.pangercic@gmail.com>
2011-01-12 21:00:10 +0100

    time for feature extraction

--- color_feature_classification/scripts/estimateGRSD.sh
+++ color_feature_classification/scripts/estimateGRSD.sh
@@ -1,13 +1,18 @@
 #!/bin/bash
 
 DATA=`pwd`
-files=`find $DATA -type f \( -iname "*$1*.pcd" \)`
+files=`find $DATA -type f \( -iname "*.pcd" \)`
 radii=`echo _radii.pcd`
+
 
 for j in $files
 do
     newname=`basename $j .pcd`
-    echo "Object: " $newname
-    rosrun color_chlac exampleGRSD $j 
-    mv $DATA/radii.pcd $DATA/$newname$radii
+    rosrun color_chlac test_GRSD_CCHLAC $j 
+#    mv $DATA/radii.pcd $DATA/$newname$radii
 done
+
+#estimate feature extraction time
+#before you have to define VERBOSE 1 in color_chlac/test/example_GRSD_CCHLAC.cpp
+#Command:
+#bash /home/pangerci/ros/tum-stacks/mapping-private/color_feature_classification/scripts/estimateGRSD.sh | awk -F ' ' {'print $5'} >> time.txt
