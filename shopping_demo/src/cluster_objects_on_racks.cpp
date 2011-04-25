68045c4954290d5323c01e6ceb2af540b50de1e1
Lucian Cosmin Goron <goron@cs.tum.edu>
2011-04-25 14:21:29 +0200

    in <shopping_demo> update <#include> files

--- shopping_demo/src/cluster_objects_on_racks.cpp
+++ shopping_demo/src/cluster_objects_on_racks.cpp
@@ -29,53 +29,14 @@
 
 
 
-//#include "pcl_ros/publisher.h"
-//#include "pcl_ros/subscriber.h"
-
-//#include <tf/transform_listener.h>
-
-
 // ros dependencies
 #include "ros/ros.h"
 
-//// terminal tools dependecies
-//#include "terminal_tools/parse.h"
-
 // pcl dependencies
 #include "pcl/io/pcd_io.h"
-#include "pcl/point_types.h"
-//#include "pcl_ros/transforms.h"
-
-#include <pcl/features/normal_3d.h>
-//#include "pcl/surface/convex_hull.h"
-
-//#include "pcl/filters/voxel_grid.h"
-//#include "pcl/filters/passthrough.h"
+#include "pcl/kdtree/kdtree_flann.h"
 #include "pcl/filters/extract_indices.h"
-//#include "pcl/filters/project_inliers.h"
-//#include "pcl/filters/statistical_outlier_removal.h"
-
-#include "pcl/sample_consensus/method_types.h"
-//#include "pcl/sample_consensus/impl/ransac.hpp"
-
-#include "pcl/segmentation/sac_segmentation.h"
 #include "pcl/segmentation/extract_clusters.h"
-//#include "pcl/segmentation/extract_polygonal_prism_data.h"
-
-//// pcl visualization dependencies
-//#include "pcl_visualization/pcl_visualizer.h"
-
-//// pcl ias sample consensus dependencies
-//#include "pcl_ias_sample_consensus/pcl_sac_model_orientation.h"
-
-//// dos pcl dependencies
-//#include "dos_pcl/segmentation/door_detection_by_color_and_fixture.h"
-//#include "dos_pcl_ros/segmentation/door_detection_by_color_and_fixture.h"
-
-
-
-
-//using namespace std;
 
 
 
