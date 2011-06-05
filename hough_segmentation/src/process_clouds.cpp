28aa233c39eac477f3c776df3c6c865f198b19c9
Lucian Cosmin Goron <goron@in.tum.de>
2011-06-05 15:21:28 +0200

    in <hough_segmentation> WIP processing point clouds

--- hough_segmentation/src/process_clouds.cpp
+++ hough_segmentation/src/process_clouds.cpp
@@ -27,7 +27,9 @@
  * POSSIBILITY OF SUCH DAMAGE.
  */
 
-
+// ------------------------------------------------------------------------- //
+// -------------------- Specify all needed dependencies -------------------- //
+// ------------------------------------------------------------------------- //
 
 // ros dependencies
 #include "ros/ros.h"
@@ -44,46 +46,58 @@
 #include "pcl/filters/statistical_outlier_removal.h"
 #include "pcl/sample_consensus/method_types.h"
 #include "pcl/sample_consensus/sac_model_circle.h"
+#include "pcl/surface/convex_hull.h"
 #include "pcl/segmentation/sac_segmentation.h"
 #include "pcl/segmentation/extract_clusters.h"
+#include "pcl/segmentation/extract_polygonal_prism_data.h"
 
 // pcl visualization dependencies
 #include "pcl_visualization/pcl_visualizer.h"
 
-
+// ---------------------------------------------------------------------- //
+// -------------------- Declare definitions of types -------------------- //
+// ---------------------------------------------------------------------- //
 
 typedef pcl::PointXYZINormal PointT;
 
+// --------------------------------------------------------------------------- //
+// -------------------- Declare the processing parameters -------------------- //
+// --------------------------------------------------------------------------- //
 
+// Options for processing
+bool pass = false; 
+bool table = false; 
+bool filter = false; 
+bool normal = false; 
+bool refine = false; 
+bool project = false; 
 
-bool pass = true; 
-bool table = true; 
-bool filter = true; 
-bool normal = true; 
-bool refine = true; 
-bool project = true; 
-
+// Pass through filter's parameters
 double height = 0.500; /// [meters]
 
+// Plane fitting's parameters
 int weight = 0.100; /// [meters]
 double epsilon = 0.100; /// [radians]
 int iterations = 1000; /// [iterations]
 double threshold = 0.100; /// [meters]
 
+// Visualization's parameters
 bool verbose = false;
 int size_of_points = 3;
 
 
 
-/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-/** \brief Main routine of the method. Processing point clouds. Projection in 2D. Computing of normals in 2D. Refinement of normals in 2D.
+////////////////////////////////////////////////////////////////////////////////////////////////////
+/** \brief Main routine of the method. Processing point clouds. Pass through filtering.
+ * Objects segmentation of the table. Statistical noise removal. Computation of normals in 2D.
+ * Refinement of normals in 2D. Projection of points in 2D.
  */
 int main (int argc, char** argv)
 {
- 
-  // --------------------------------------------------------------- //
-  // ------------------ Check and parse arguments ------------------ //
-  // --------------------------------------------------------------- //
+
+  // ------------------------------------------------------------------------- //
+  // -------------------- Check and parse arguments -------------------- //
+  // ------------------------------------------------------------------------- //
 
   // Argument check and info about
   if (argc < 2)
@@ -91,12 +105,6 @@ int main (int argc, char** argv)
     ROS_INFO (" ");
     ROS_INFO ("Syntax is: %s <input>.pcd <options>", argv[0]);
     ROS_INFO ("  where <options> are:");
-    ROS_INFO ("    -height X                   = ");
-    ROS_INFO (" ");
-    ROS_INFO ("    -weight X              = ");
-    ROS_INFO ("    -epsilon X             = The maximum allowed difference between the plane normal and the given axis");
-    ROS_INFO ("    -threshold X           = Distance to the fitted plane model");
-    ROS_INFO ("    -iterations X          = Maximum number of interations for fitting the plane model");
     ROS_INFO (" ");
     ROS_INFO ("    -pass B                = Yes/No to passing through filter");
     ROS_INFO ("    -table B               = Yes/No to segmenting the objects on the table");
@@ -105,8 +113,15 @@ int main (int argc, char** argv)
     ROS_INFO ("    -refine B              = Yes/No to refinement of normals in 2D");
     ROS_INFO ("    -project B             = Yes/No to projecting points in xOy plane");
     ROS_INFO (" ");
+    ROS_INFO ("    -height X              = The height of pass through filter");
+    ROS_INFO (" ");
+    ROS_INFO ("    -weight X              = The weight of angular distance between point normals and the normal of plane");
+    ROS_INFO ("    -epsilon X             = The maximum allowed difference between the plane normal and the given axis");
+    ROS_INFO ("    -threshold X           = The distance to the fitted plane model");
+    ROS_INFO ("    -iterations X          = The maximum number of interations for fitting the plane model");
+    ROS_INFO (" ");
     ROS_INFO ("    -verbose B             = Yes/No to printing of useful messages");
-    ROS_INFO ("    -size_of_points D      = Size of points for 3D viewer");
+    ROS_INFO ("    -size_of_points D      = The size of points in 3D viewer");
     ROS_INFO (" ");
     return (-1);
   }
@@ -119,6 +134,7 @@ int main (int argc, char** argv)
     return (-1);
   }
 
+  // Options for processing
   terminal_tools::parse_argument (argc, argv, "-pass", pass);
   terminal_tools::parse_argument (argc, argv, "-table", table);
   terminal_tools::parse_argument (argc, argv, "-filter", filter);
@@ -126,19 +142,22 @@ int main (int argc, char** argv)
   terminal_tools::parse_argument (argc, argv, "-refine", refine);
   terminal_tools::parse_argument (argc, argv, "-project", project);
 
+  // Pass through filter's parameters
   terminal_tools::parse_argument (argc, argv, "-height", height);
 
+  // Plane fitting's parameters
   terminal_tools::parse_argument (argc, argv, "-weight", weight);
   terminal_tools::parse_argument (argc, argv, "-epsilon", epsilon);
   terminal_tools::parse_argument (argc, argv, "-threshold", threshold);
   terminal_tools::parse_argument (argc, argv, "-iterations", iterations);
 
+  // Visualization's parameters
   terminal_tools::parse_argument (argc, argv, "-verbose", verbose);
   terminal_tools::parse_argument (argc, argv, "-size_of_points", size_of_points);
 
-  // ----------------------------------------------------- //
-  // ------------------ Initializations ------------------ //
-  // ----------------------------------------------------- //
+  // --------------------------------------------------------- //
+  // -------------------- Initializations -------------------- //
+  // --------------------------------------------------------- //
 
   // Initialize random number generator
   srand (time(0));
@@ -158,9 +177,9 @@ int main (int argc, char** argv)
     ROS_WARN ("Timer started !");
   }
 
-  // ----------------------------------------------------------- //
-  // ------------------ Load point cloud data ------------------ //
-  // ----------------------------------------------------------- //
+  // ------------------------------------------------------------------------- //
+  // -------------------- Load point cloud data -------------------- //
+  // ------------------------------------------------------------------------- //
 
   // Input point cloud data
   pcl::PointCloud<PointT>::Ptr input_cloud (new pcl::PointCloud<PointT> ());
@@ -183,9 +202,9 @@ int main (int argc, char** argv)
   // Update working point cloud
   *working_cloud = *input_cloud;
 
-  // ---------------------------------------------------------------- //
-  // ------------------ Visualize point cloud data ------------------ //
-  // ---------------------------------------------------------------- //
+  // -------------------------------------------------------------------- //
+  // -------------------- Visualize point cloud data -------------------- //
+  // -------------------------------------------------------------------- //
 
   // Open a 3D viewer
   pcl_visualization::PCLVisualizer viewer ("3D VIEWER");
@@ -205,9 +224,9 @@ int main (int argc, char** argv)
   // And wait until Q key is pressed
   viewer.spin ();
 
-  // --------------------------------------------------------- //
-  // ------------------ Pass Through Filter ------------------ //
-  // --------------------------------------------------------- //
+  // -------------------------------------------------------------- //
+  // -------------------- Pass Through Filter  -------------------- //
+  // -------------------------------------------------------------- //
 
   if ( pass )
   {
@@ -230,7 +249,7 @@ int main (int argc, char** argv)
     pass.setFilterFieldName ("z");
     pass.setFilterLimits (0.0, height);
     pass.setInputCloud (working_cloud);
-  
+
     // Call the filtering function
     pass.setFilterLimitsNegative (false);
     pass.filter (*through);
@@ -255,9 +274,9 @@ int main (int argc, char** argv)
     viewer.spin ();
   }
 
-  // ----------------------------------------------------------------------- //
-  // ------------------ Segmenting Objects from the Table ------------------ //
-  // ----------------------------------------------------------------------- //
+  // ---------------------------------------------------------------------------- //
+  // -------------------- Segmenting Objects from the Table  -------------------- //
+  // ---------------------------------------------------------------------------- //
 
   if ( table )
   {
@@ -328,10 +347,6 @@ int main (int argc, char** argv)
     // Call the extraction function
     extraction.filter (*working_cloud);
 
-
-
-
-
     // Create ID for visualization
     std::stringstream id;
     id << "INLIERS_" << ros::Time::now();
@@ -339,7 +354,85 @@ int main (int argc, char** argv)
     // Add point cloud to viewer
     viewer.addPointCloud (*cloud, id.str());
     // Set the size of points for cloud
-    viewer.setPointCloudRenderingProperties (pcl_visualization::PCL_VISUALIZER_POINT_SIZE, size_of_points, id.str()); 
+    viewer.setPointCloudRenderingProperties (pcl_visualization::PCL_VISUALIZER_POINT_SIZE, size_of_points, id.str ()); 
+    // And wait until Q key is pressed
+    viewer.spin ();
+
+
+
+
+
+    pcl::PointIndices::Ptr over (new pcl::PointIndices ());
+    pcl::PointIndices::Ptr under (new pcl::PointIndices ());
+
+    float a = coefficients->values [0];
+    float b = coefficients->values [1];
+    float c = coefficients->values [2];
+    float d = coefficients->values [3];
+
+
+
+    for (int idx = 0; idx < (int) working_cloud->points.size (); idx++)
+    {
+
+
+
+      float x = working_cloud->points [idx].x;
+      float y = working_cloud->points [idx].y;
+      float z = working_cloud->points [idx].z;
+
+
+
+      float s = (a*x) + (b*y) + (c*z) + d;
+
+
+      //cerr << " s = " << s << endl ;
+
+      if ( s < 0 ) 
+      {
+        //cerr << " idx " << idx <<  " bla " << endl ;
+        over->indices.push_back (idx);
+      }
+      else
+      {
+        //cerr << " idx " << idx << " alb " << endl ;
+        under->indices.push_back (idx);
+      }
+
+      //cerr << " s = " << s << endl ;
+    }
+
+
+
+
+
+
+    // Extract the inliers from the working cloud
+    pcl::ExtractIndices<PointT> extraction_;
+    pcl::PointCloud<PointT>::Ptr cloud_ (new pcl::PointCloud<PointT> ());
+
+    // Set point cloud from where to extract
+    extraction_.setInputCloud (working_cloud);
+    // Set which indices to extract
+    extraction_.setIndices (over);
+
+    // Return the points which represent the inliers
+    extraction_.setNegative (false);
+    // Call the extraction function
+    extraction_.filter (*cloud_);
+    // Return the remaining points of inliers
+    extraction_.setNegative (true);
+    // Call the extraction function
+    extraction_.filter (*working_cloud);
+
+    // Create ID for visualization
+    std::stringstream id_;
+    id_ << "INLIERS_" << ros::Time::now();
+
+    // Add point cloud to viewer
+    viewer.addPointCloud (*cloud_, id_.str());
+    // Set the size of points for cloud
+    viewer.setPointCloudRenderingProperties (pcl_visualization::PCL_VISUALIZER_POINT_SIZE, size_of_points, id_.str ()); 
     // And wait until Q key is pressed
     viewer.spin ();
 
@@ -348,34 +441,22 @@ int main (int argc, char** argv)
 
 
 
+    // Get position of dot in path of file 
+    std::string file = argv [pFileIndicesPCD[0]];
+    size_t dot = file.find (".");
 
-//      pcl::PointCloud<PointT> cloud_projected;
-//
-      pcl::PointCloud<PointT>::Ptr projected_cloud (new pcl::PointCloud<PointT> ()); 
+    // Create file name for saving
+    std::string name = argv [pFileIndicesPCD [0]];
+    name.insert (dot, "-objects");
 
-      // Project the table inliers using the planar model coefficients    
-      pcl::ProjectInliers<PointT> projecting;   
+    // Save these points to disk
+    pcl::io::savePCDFile (name, *cloud_);
 
-      proj_.setModelType (pcl::SACMODEL_NORMAL_PLANE);
-      proj_.setInputCloud (planar_surfaces.at (surface));
-      proj_.setModelCoefficients (planar_surfaces_coefficients.at (surface));
-      proj_.filter (*cloud_projected);
-
-      // // // // // //
-      // Save all points of planar patches and handles which make up the furniture
-      // // // // // //
-//      *furniture += cloud_projected;
-      // pcl::ProjectInliers DOES NOT KEEP RGB INFORMATION
-
-      // Remove the point cloud data
-      viewer.removePointCloud (planar_surfaces_ids.at (surface));
-
-      // Wait or not wait
-      if ( step )
-      {
-        // And wait until Q key is pressed
-        viewer.spin ();
-      }
+    if ( verbose )
+    {
+      // Show the objects' number of points
+      ROS_INFO ("Message %d points !", (int) cloud_->points.size ());
+    }
 
 
 
@@ -386,6 +467,111 @@ int main (int argc, char** argv)
 
 
 
+
+
+
+
+
+
+
+/*
+    // Project the table inliers using the planar model coefficients    
+    pcl::ProjectInliers<PointT> projecting;   
+    pcl::PointCloud<PointT>::Ptr projected_cloud (new pcl::PointCloud<PointT> ()); 
+
+    // Set all the parameters for projecting 
+    projecting.setModelType (pcl::SACMODEL_NORMAL_PLANE);
+    projecting.setInputCloud (cloud);
+    projecting.setModelCoefficients (coefficients);
+    projecting.filter (*projected_cloud);
+
+
+
+    // Remove the point cloud data
+    viewer.removePointCloud (id.str ());
+    // And wait until Q key is pressed
+    viewer.spin ();
+
+
+
+    // Visualize Projecte Poitns
+    std::stringstream id_of_proj;
+    id_of_proj << "PROJ_" << ros::Time::now();
+
+    // Add point cloud to viewer
+    viewer.addPointCloud (*projected_cloud, id_of_proj.str());
+    // Set the size of points for cloud
+    viewer.setPointCloudRenderingProperties (pcl_visualization::PCL_VISUALIZER_POINT_SIZE, size_of_points * 2, id_of_proj.str());
+    // And wait until Q key is pressed
+    viewer.spin ();
+
+
+
+    // Create a Convex Hull representation of the projected inliers
+    pcl::ConvexHull<PointT> chull_;  
+    pcl::PointCloud<PointT> cloud_hull;
+
+    chull_.setInputCloud (projected_cloud);
+    ROS_ERROR (" BEFORE IT WORKS ");
+    chull_.reconstruct (cloud_hull);      
+    ROS_ERROR (" AFTERWARDS NOT ");
+
+    ROS_INFO ("Convex hull has %d data points.", (int) cloud_hull.points.size ());
+
+
+
+    // Visualize Convex Hulls
+    std::stringstream id_of_hull;
+    id_of_hull << "HULL_" << ros::Time::now();
+
+    // Visualize hull of cloud 
+    viewer.addPointCloud (cloud_hull, id_of_hull.str());
+    // Set the size of points for cloud
+    viewer.setPointCloudRenderingProperties (pcl_visualization::PCL_VISUALIZER_POINT_SIZE, size_of_points, id_of_hull.str());     
+    // And wait until Q key is pressed
+    viewer.spin ();
+
+
+
+    pcl::PointIndices::Ptr cloud_object_indices (new pcl::PointIndices ());
+
+    // ---[ Get the objects on top of the table
+    pcl::ExtractPolygonalPrismData<PointT> prism_;
+    prism_.setHeightLimits (0.000, 0.500);
+    prism_.setInputCloud (input_cloud);
+    prism_.setInputPlanarHull (cloud_hull.makeShared());
+    prism_.setViewPoint (-1.0, -1.0, 1.0);
+    prism_.segment (*cloud_object_indices);
+
+    if ( (int) cloud_object_indices->indices.size () != 0 )
+    {
+      // Extract objects
+      //pcl::PointCloud<PointT> object_cloud;
+      pcl::PointCloud<PointT>::Ptr object_cloud (new pcl::PointCloud<PointT> ()); 
+      pcl::ExtractIndices<PointT> extraction_of_object;
+      // Set point cloud from where to extract
+      extraction_of_object.setInputCloud (input_cloud);
+      // Set which indices to extract
+      extraction_of_object.setIndices (cloud_object_indices);
+      // Return the points which represent the inliers
+      extraction_of_object.setNegative (false);
+      // Call the extraction function
+      extraction_of_object.filter (*object_cloud);
+
+
+
+      // Visualize objects
+      std::stringstream id_of_object;
+      id_of_object << "OBJECTS_" << ros::Time::now();
+
+      // Visualize object
+      viewer.addPointCloud (*object_cloud, id_of_object.str());
+      // Set the size of points for cloud
+      viewer.setPointCloudRenderingProperties (pcl_visualization::PCL_VISUALIZER_POINT_SIZE, size_of_points * 3, id_of_object.str());     
+      // And wait until Q key is pressed
+      viewer.spin ();
+    }
+*/
   }
 
 
@@ -404,14 +590,9 @@ int main (int argc, char** argv)
 
 
 
-
-
-
-
-
-  // -------------------------------------------------------------- //
-  // ------------------ Filtering of Point Cloud ------------------ //
-  // -------------------------------------------------------------- //
+  // ------------------------------------------------------------------- //
+  // -------------------- Filtering of Point Cloud  -------------------- //
+  // ------------------------------------------------------------------- //
 
   if ( filter )
   {
@@ -443,9 +624,9 @@ int main (int argc, char** argv)
     viewer.spin ();
   }
 
-  // ---------------------------------------------------------------- //
-  // ------------------ Projecting of Point Clouds ------------------ //
-  // ---------------------------------------------------------------- //
+  // -------------------------------------------------------------------- //
+  // -------------------- Projecting of Point Clouds -------------------- //
+  // -------------------------------------------------------------------- //
 
   if ( project )
   {
@@ -460,9 +641,9 @@ int main (int argc, char** argv)
     viewer.spin ();
   }
 
-  // ------------------------------------------------------------------ //
-  // ------------------ Estimating Normals of Points ------------------ //
-  // ------------------------------------------------------------------ //
+  // ---------------------------------------------------------------------- //
+  // -------------------- Estimating Normals of Points -------------------- //
+  // ---------------------------------------------------------------------- //
 
   if ( normal )
   {
@@ -507,9 +688,9 @@ int main (int argc, char** argv)
 
 
 
-    // --------------------------------------------------------------------- //
-    // ------------------ Refinement of Normals of Points ------------------ //
-    // --------------------------------------------------------------------- //
+    // ------------------------------------------------------------------------- //
+    // -------------------- Refinement of Normals of Points -------------------- //
+    // ------------------------------------------------------------------------- //
 
     if ( refine )
     {
