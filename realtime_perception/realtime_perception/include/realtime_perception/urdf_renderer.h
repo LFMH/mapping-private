93adf06748c5c2e89add6036abc1fce902b35520
Nico Blodow <blodow@cs.tum.edu>
2011-10-25 13:57:05 +0200

    cleaned up urdf_renderer

--- realtime_perception/realtime_perception/include/realtime_perception/urdf_renderer.h
+++ realtime_perception/realtime_perception/include/realtime_perception/urdf_renderer.h
@@ -15,14 +15,14 @@ class URDFRenderer
 { 
   public:
     URDFRenderer (std::string model_description, std::string tf_prefix, std::string cam_frame, std::string fixed_frame, tf::TransformListener &tf);
+    void render ();
+
+  protected:
     void initURDFModel ();
     void loadURDFModel (urdf::Model &descr);
     void process_link (boost::shared_ptr<urdf::Link> link);
-    void transforms_changed ();
-    void render ();
-    void setCameraFrame (std::string &cam_f);
+    void update_link_transforms ();
 
-  private:
     // urdf model stuff
     std::string model_description_;
     std::string tf_prefix_;
