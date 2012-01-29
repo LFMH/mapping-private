a27a1011fd3a1671e0bb17b49293c2386441cd23
Lucian Cosmin Goron <goron@in.tum.de>
2012-01-29 02:01:17 +0200

    transforming two specific point clouds

--- hough_segmentation/src/rot_trans_scale.cpp
+++ hough_segmentation/src/rot_trans_scale.cpp
@@ -52,15 +52,15 @@ int size = 1;
 
 // Control //
 bool transform = false;
-bool again = false;
+int again = false;
 
 // Transforming //
-double x = 0.0;
-double y = 0.0;
-double z = 0.0;
-double roll = 0.0;
-double pitch = 0.0;
-double yaw = 0.0;
+float x = 0.0;
+float y = 0.0;
+float z = 0.0;
+float roll = 0.0;
+float pitch = 0.0;
+float yaw = 0.0;
 
 // ---------- Macros ---------- //
 
@@ -176,7 +176,7 @@ int main (int argc, char** argv)
   // ---------- 3D Viewer ---------- //
 
   pcl::visualization::PCLVisualizer viewer ("3D VIEWER");
-  viewer.setBackgroundColor (0.0, 0.0, 0.0);
+  viewer.setBackgroundColor (1.0, 1.0, 1.0);
   viewer.addCoordinateSystem (1.0f);
   viewer.getCameraParameters (argc, argv);
   viewer.updateCamera ();
@@ -195,11 +195,13 @@ int main (int argc, char** argv)
 
   if ( step )
   {
-    pcl::visualization::PointCloudColorHandlerCustom<PointT> input_color (input_cloud, 255, 255, 255);
+    pcl::visualization::PointCloudColorHandlerCustom<PointT> input_color (input_cloud, 0, 0, 0);
     viewer.addPointCloud<PointT> (input_cloud, input_color, "generic");
     viewer.setPointCloudRenderingProperties (pcl::visualization::PCL_VISUALIZER_POINT_SIZE, size, "generic");
-    viewer.spin ();
+    // viewer.spin ();
   }
+
+  /*
 
   // ---------- Add Plane ---------- //
 
@@ -211,8 +213,172 @@ int main (int argc, char** argv)
     coeffs.values.push_back (1.0);
     coeffs.values.push_back (0.0);
     viewer.addPlane (coeffs, 0.0, 1.0, 1.0, "plane");
-    viewer.spin ();
+    //viewer.spin ();
   }
+
+  */
+
+  /*
+
+  // ---------- Add Cuboid ---------- //
+
+  pcl::ModelCoefficients e0, e1, e2, e3;
+
+  e0.values.push_back (-0.900139);
+  e0.values.push_back (-1.16862);
+  e0.values.push_back (0.443211);
+  e0.values.push_back (-0.0269661);
+  e0.values.push_back (-0.0386442);
+  e0.values.push_back (0);
+
+  e1.values.push_back (-0.927105);
+  e1.values.push_back (-1.20727);
+  e1.values.push_back (0.443211);
+  e1.values.push_back (0.111587);
+  e1.values.push_back (-0.0778655);
+  e1.values.push_back (0);
+
+  e2.values.push_back (-0.815519);
+  e2.values.push_back (-1.28513);
+  e2.values.push_back (0.443211);
+  e2.values.push_back (0.0269661);
+  e2.values.push_back (0.0386442);
+  e2.values.push_back (0);
+
+  e3.values.push_back (-0.788553);
+  e3.values.push_back (-1.24649);
+  e3.values.push_back (0.443211);
+  e3.values.push_back (-0.111587);
+  e3.values.push_back (0.0778655);
+  e3.values.push_back (0);
+
+  pcl::ModelCoefficients e4, e5, e6, e7;
+
+  e4.values.push_back (-0.900139);
+  e4.values.push_back (-1.16862);
+  e4.values.push_back (0.632181);
+  e4.values.push_back (-0.0269661);
+  e4.values.push_back (-0.0386442);
+  e4.values.push_back (0);
+
+  e5.values.push_back (-0.927105);
+  e5.values.push_back (-1.20727);
+  e5.values.push_back (0.632181);
+  e5.values.push_back (0.111587);
+  e5.values.push_back (-0.0778655);
+  e5.values.push_back (0);
+
+  e6.values.push_back (-0.815519);
+  e6.values.push_back (-1.28513);
+  e6.values.push_back (0.632181);
+  e6.values.push_back (0.0269661);
+  e6.values.push_back (0.0386442);
+  e6.values.push_back (0);
+
+  e7.values.push_back (-0.788553);
+  e7.values.push_back (-1.24649);
+  e7.values.push_back (0.632181);
+  e7.values.push_back (-0.111587);
+  e7.values.push_back (0.0778655);
+  e7.values.push_back (0);
+
+  std::vector<pcl::ModelCoefficients> cub;
+
+  cub.push_back (e0);
+  cub.push_back (e1);
+  cub.push_back (e2);
+  cub.push_back (e3);
+  cub.push_back (e4);
+  cub.push_back (e5);
+  cub.push_back (e6);
+  cub.push_back (e7);
+
+  std::stringstream cub_id;
+  cub_id << "CUB_" << getTimestamp ();
+  viewer.addCuboid (cub, 0.5, 0.0, 1.0, 0.5, cub_id.str ());
+
+  */
+
+  /*
+
+  // ---------- Add Cuboid ---------- //
+
+  pcl::ModelCoefficients e0, e1, e2, e3;
+
+  e0.values.push_back (-0.440659);
+  e0.values.push_back (-1.25178);
+  e0.values.push_back (0.441576);
+  e0.values.push_back (-0.0192634);
+  e0.values.push_back (0.0785663);
+  e0.values.push_back (0);
+
+  e1.values.push_back (-0.459923);
+  e1.values.push_back (-1.17321);
+  e1.values.push_back (0.441576);
+  e1.values.push_back (-0.137991);
+  e1.values.push_back (-0.0338334);
+  e1.values.push_back (0);
+
+  e2.values.push_back (-0.597914);
+  e2.values.push_back (-1.20704);
+  e2.values.push_back (0.441576);
+  e2.values.push_back (0.0192634);
+  e2.values.push_back (-0.0785663);
+  e2.values.push_back (0);
+
+  e3.values.push_back (-0.57865);
+  e3.values.push_back (-1.28561);
+  e3.values.push_back (0.441576);
+  e3.values.push_back (0.137991);
+  e3.values.push_back (0.0338334);
+  e3.values.push_back (0);
+
+  pcl::ModelCoefficients e4, e5, e6, e7;
+
+  e4.values.push_back (-0.440659);
+  e4.values.push_back (-1.25178);
+  e4.values.push_back (0.636602);
+  e4.values.push_back (-0.0192634);
+  e4.values.push_back (0.0785663);
+  e4.values.push_back (0);
+
+  e5.values.push_back (-0.459923);
+  e5.values.push_back (-1.17321);
+  e5.values.push_back (0.636602);
+  e5.values.push_back (-0.137991);
+  e5.values.push_back (-0.0338334);
+  e5.values.push_back (0);
+
+  e6.values.push_back (-0.597914);
+  e6.values.push_back (-1.20704);
+  e6.values.push_back (0.636602);
+  e6.values.push_back (0.0192634);
+  e6.values.push_back (-0.0785663);
+  e6.values.push_back (0);
+
+  e7.values.push_back (-0.57865);
+  e7.values.push_back (-1.28561);
+  e7.values.push_back (0.636602);
+  e7.values.push_back (0.137991);
+  e7.values.push_back (0.0338334);
+  e7.values.push_back (0);
+
+  std::vector<pcl::ModelCoefficients> cub;
+
+  cub.push_back (e0);
+  cub.push_back (e1);
+  cub.push_back (e2);
+  cub.push_back (e3);
+  cub.push_back (e4);
+  cub.push_back (e5);
+  cub.push_back (e6);
+  cub.push_back (e7);
+
+  std::stringstream cub_id;
+  cub_id << "CUB_" << getTimestamp ();
+  viewer.addCuboid (cub, 0.5, 0.0, 1.0, 0.5, cub_id.str ());
+
+  */
 
   // ---------- Introduce Working Cloud ---------- //
 
@@ -221,6 +387,8 @@ int main (int argc, char** argv)
   pcl::copyPointCloud (*input_cloud, *working_cloud);
 
   // ---------- Transforming Point Cloud ---------- //
+
+  again = 0;
 
   if ( transform )
   {
@@ -235,14 +403,23 @@ int main (int argc, char** argv)
       if ( step )
       {
         viewer.removePointCloud ("generic");
-        pcl::visualization::PointCloudColorHandlerCustom<PointT> working_color (working_cloud, 255, 255, 255);
+        pcl::visualization::PointCloudColorHandlerCustom<PointT> working_color (working_cloud, 0, 0, 0);
         viewer.addPointCloud<PointT> (working_cloud, working_color, "generic");
         viewer.setPointCloudRenderingProperties (pcl::visualization::PCL_VISUALIZER_POINT_SIZE, size, "generic");
         viewer.spin ();
       }
 
-      printf ("Again ? 0/1 = ");
-      scanf ("%d", &again);
+      // printf ("Again? 0/1 = "); scanf ("%d", &again);
+
+      if ( again == 1 )
+      {
+        printf ("x = "); scanf ("%f", &x);
+        printf ("y = "); scanf ("%f", &y);
+        printf ("z = "); scanf ("%f", &z);
+        printf (" roll = "); scanf ("%f", &roll);
+        printf ("pitch = "); scanf ("%f", &pitch);
+        printf ("  yaw = "); scanf ("%f", &yaw);
+      }
 
     } while ( again == 1 );
   }
@@ -252,12 +429,13 @@ int main (int argc, char** argv)
 
   pcl::io::savePCDFile (argv [pcd_file_indices [1]], *working_cloud, 10);
 
+  if ( verbose ) pcl::console::print_info ("Saved %d points to %s with following fields: %s\n", (int) (working_cloud->points.size ()), argv [pcd_file_indices [1]], pcl::getFieldsList (*working_cloud).c_str ());
 
   // ---------- The End ---------- //
 
   if ( verbose ) pcl::console::print_warn ("Finished in %g [ms] !\n", tt.toc ());
 
-  if ( step ) viewer.spin ();
+  // if ( step ) viewer.spin ();
 
   return (0);
 }
